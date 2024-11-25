79f716e9165213137bbd2ddeaa51e68004208887
winechit-nfq <wine.chit@nfq.com>
2024-11-25 15:15:32 +0700

    Add ThemePreviews

--- app/src/main/java/com/nfq/nfqsummit/ui/theme/Theme.kt
+++ app/src/main/java/com/nfq/nfqsummit/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.nfq.nfqsummit.ui.theme
 
 import android.app.Activity
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 
 private val DarkColorPalette = darkColorScheme(
@@ -112,3 +114,7 @@ fun Surface(
         content()
     }
 }
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, name = "Light theme")
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, name = "Dark theme")
+annotation class ThemePreviews
