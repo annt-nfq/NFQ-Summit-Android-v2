ca55ea74b53ecab686cf2bbb264bc45f0f011a18
winechit-nfq <wine.chit@nfq.com>
2024-11-25 17:40:21 +0700

    Add new typography and font family

--- app/src/main/java/com/nfq/nfqsummit/ui/theme/Type.kt
+++ app/src/main/java/com/nfq/nfqsummit/ui/theme/Type.kt
@@ -4,16 +4,28 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.nfq.nfqsummit.R
 
 val notoSansFamily = FontFamily(
     Font(R.font.notosans_regular, FontWeight.Normal),
+    Font(R.font.notosans_medium, FontWeight.Medium),
     Font(R.font.notosans_bold, FontWeight.Bold),
 )
 val Typography = Typography(
+    bodySmall = TextStyle(
+        fontFamily = notoSansFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 18.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = notoSansFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 19.07.sp
+    ),
     bodyLarge = TextStyle(
         fontFamily = notoSansFamily,
         fontWeight = FontWeight.Normal,
