13a09b064cdb903f62f1ef4de2e6d6718b1227b3
winechit-nfq <wine.chit@nfq.com>
2024-11-25 14:15:18 +0700

    Update font family

--- app/src/main/java/com/nfq/nfqsummit/ui/theme/Type.kt
+++ app/src/main/java/com/nfq/nfqsummit/ui/theme/Type.kt
@@ -9,100 +9,97 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.nfq.nfqsummit.R
 
-val montserratFamily = FontFamily(
-    Font(R.font.montserrat_regular, FontWeight.Normal),
-    Font(R.font.montserrat_medium, FontWeight.Medium),
-    Font(R.font.montserrat_semi_bold, FontWeight.SemiBold),
-    Font(R.font.montserrat_bold, FontWeight.Bold),
-    Font(R.font.montserrat_italic, FontWeight.Normal, FontStyle.Italic),
+val notoSansFamily = FontFamily(
+    Font(R.font.notosans_regular, FontWeight.Normal),
+    Font(R.font.notosans_bold, FontWeight.Bold),
 )
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 13.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 18.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
     displayMedium = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
     displayLarge = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 56.sp,
         lineHeight = 64.sp,
         letterSpacing = 0.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = montserratFamily,
+        fontFamily = notoSansFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 18.sp,
         lineHeight = 24.sp,
