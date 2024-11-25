4faa08647d135c00428f6faa8c253f880cae745f
winechit-nfq <wine.chit@nfq.com>
2024-11-25 17:49:41 +0700

    Add NetworkImagePainter.kt

---
+++ app/src/main/java/com/nfq/nfqsummit/components/NetworkImagePainter.kt
@@ -0,0 +1,21 @@
+package com.nfq.nfqsummit.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.nfq.nfqsummit.R
+
+@Composable
+fun networkImagePainter(
+    url: String?,
+): Painter = rememberAsyncImagePainter(
+    model = ImageRequest.Builder(LocalContext.current)
+        .data(url)
+        .crossfade(true)
+        .error(R.drawable.ic_placeholder_default)
+        .placeholder(R.drawable.ic_placeholder_default)
+        .size(coil.size.Size.ORIGINAL)
+        .build(),
+)
