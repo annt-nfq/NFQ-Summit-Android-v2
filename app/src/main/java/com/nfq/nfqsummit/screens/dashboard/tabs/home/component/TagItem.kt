4b1223f53f8bc73016d8a21b4642072edd1182f0
winechit-nfq <wine.chit@nfq.com>
2024-12-27 16:28:53 +0700

    - Replace with AutoResizedText

--- app/src/main/java/com/nfq/nfqsummit/screens/dashboard/tabs/home/component/TagItem.kt
+++ app/src/main/java/com/nfq/nfqsummit/screens/dashboard/tabs/home/component/TagItem.kt
@@ -5,13 +5,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nfq.nfqsummit.components.AutoResizedText
 import com.nfq.nfqsummit.ui.theme.NFQSnapshotTestThemeForPreview
 
 @Composable
@@ -29,11 +30,14 @@ fun TagItem(
             )
             .padding(horizontal = 12.dp, vertical = 4.dp)
     ) {
-        Text(
+        AutoResizedText(
             text = tag,
-            style = MaterialTheme.typography.bodySmall,
+            style = MaterialTheme.typography.bodySmall.copy(
+                fontWeight = FontWeight.Medium
+            ),
+            minFontSize = 10.sp,
+            maxLines = 1,
             color = contentColor,
-            fontWeight = FontWeight.Medium
         )
     }
 }
