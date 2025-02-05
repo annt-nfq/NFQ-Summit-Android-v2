de5c8b49c3c51f76af191575054a2a4ea6757def
winechit-nfq <wine.chit@nfq.com>
2025-02-05 17:52:18 +0700

    - Release version 1.2.0(101025)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.nfq.nfqsummit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 101024
+        versionCode = 101025
         versionName = "1.2.0"
 
         testInstrumentationRunner = "com.nfq.nfqsummit.HiltTestRunner"
