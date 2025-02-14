221dd66b23d1d94ea9f3f0dda58915b73a8a9be4
winechit-nfq <wine.chit@nfq.com>
2025-02-14 16:47:37 +0700

    - Log event for release build

--- app/src/main/java/com/nfq/nfqsummit/analytics/helper/FirebaseAnalyticsHelper.kt
+++ app/src/main/java/com/nfq/nfqsummit/analytics/helper/FirebaseAnalyticsHelper.kt
@@ -3,6 +3,7 @@ package com.nfq.nfqsummit.analytics.helper
 import android.os.Bundle
 import com.nfq.nfqsummit.analytics.AnalyticsEvent
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.nfq.nfqsummit.BuildConfig
 import javax.inject.Inject
 
 internal class FirebaseAnalyticsHelper @Inject constructor(
@@ -10,11 +11,12 @@ internal class FirebaseAnalyticsHelper @Inject constructor(
 ) : AnalyticsHelper {
 
     override fun logEvent(event: AnalyticsEvent) {
-        val bundle = Bundle()
-        event.extras.forEach {
-            bundle.putString(it.key, it.value)
+        if (!BuildConfig.DEBUG) {
+            val bundle = Bundle()
+            event.extras.forEach {
+                bundle.putString(it.key, it.value)
+            }
+            firebaseAnalytics.logEvent(event.type, bundle)
         }
-
-        firebaseAnalytics.logEvent(event.type, bundle)
     }
 }
