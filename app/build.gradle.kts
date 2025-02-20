00b7f8926d2a826b419df9a97191beba692307bd
winechit-nfq <wine.chit@nfq.com>
2025-02-20 15:43:12 +0700

    - Release version 1.2.3 (101035)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.nfq.nfqsummit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 101034
-        versionName = "1.2.2"
+        versionCode = 101035
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "com.nfq.nfqsummit.HiltTestRunner"
         vectorDrawables {
